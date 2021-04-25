b9687289f3b950fc85fcbac98cf0b089713f4ff1
Katoumat123 <benzzatot123@gmail.com>
2021-04-25 23:59:28 +0700

    apis

--- src/working/Bisection Method.js
+++ src/working/Bisection Method.js
@@ -1,11 +1,58 @@
 import React from 'react'
 import { Button,Input } from 'antd';
 import { calBisection } from './cal_all.js'
+import apis from '../Api/index'
+import Modal_Example from '../input/model'
 
 class Bisection extends React.Component {
     state ={
-        XL: "1.5",XR: "2.0", ERROR: "0.000001",result: "",EX: "x^4-13"
+        Equation:'',
+        XL: '',
+        XR: '',
+        ERROR: '',
+        result: '',
+        isModalVisible: false,
+        apiData: [],
+        hasData: false,
     }
+
+    async getData()
+    {
+        let tempData = null
+        await apis.getRoot().then(res => {tempData = res.data})
+        this.setState({apiData: tempData})
+        this.setState({hasData: true})
+        /* console.log(tempData); */
+    }
+
+    onClickOk = e =>{
+        this.setState({isModalVisible: false})
+    }
+
+    onClickInsert = e =>{
+/*         console.log(e.currentTarget);
+        console.log(e.target);
+        console.log(e.currentTarget.getAttribute('name'));
+        console.log(e.target.name); */
+        let index = e.currentTarget.getAttribute('name').split('_')
+            index = parseInt(index[1])
+            this.setState({
+                Equation: this.state.apiData[index]["equation"],
+                XL: this.state.apiData[index]["xl"],
+                XR: this.state.apiData[index]["xr"],
+                ERROR: this.state.apiData[index]["error"],
+                isModalVisible: false
+            })
+    }
+
+    onClickExample = e =>{
+        if(!this.state.hasData){
+            this.getData()
+        }
+        this.setState({isModalVisible: true})
+    }
+
+
     getEquation = e =>{
         this.setState({
             EX: e.target.value,
@@ -41,8 +88,14 @@ class Bisection extends React.Component {
         return (
             <div className="allinbisetion" >
                 <h1 className="Ontop">Bisection Method</h1>
-
                 <div>
+                <Modal_Example
+                    visible = {this.state.isModalVisible}
+                    onOk = {this.onClickOk}
+                    hasData = {this.state.hasData}
+                    apiData = {this.state.apiData}
+                    onClick = {this.onClickInsert}
+                />
                     <span>F(x) =</span>
                     <span><Input placeholder="x^4-13" onChange={this.getEquation} className="Input_1" /></span>
                 </div>
@@ -54,6 +107,7 @@ class Bisection extends React.Component {
                     <span className="Text_Input_2"> ERROR : </span>
                     <span><Input placeholder="0.000001" onChange={this.getERR} className="Input_3" /></span>
                     <span className="Poom"><Button type="primary" onClick={this.show_value} >Calculate</Button></span>
+                    <span className="Poom"><Button type="primary" onClick={this.onClickExample} >Exsample</Button></span>
                 </div>
                 <div>
                     {this.state.result}
